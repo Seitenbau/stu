ab6435bc35d3d2418b35ae334c4570a7cdfe8f3d
Nikolaus Moll <accounts@miblounge.net>
2013-05-24 10:41:21 +0200

    more DSL template clean up, fixes in templates

--- database/src/main/java/com/seitenbau/testing/dbunit/dsl/DatabaseReference.java
+++ database/src/main/java/com/seitenbau/testing/dbunit/dsl/DatabaseReference.java
@@ -1,6 +1,25 @@
 package com.seitenbau.testing.dbunit.dsl;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
 public class DatabaseReference
 {
+
+  protected <S, T> List<T> getOrCreateList(Map<S, List<T>> map, ThreadLocal<S> threadScope) 
+  {
+    final S scope = threadScope.get();
+    if (scope == null) {
+      throw new RuntimeException("Cannot build reference out of a scope");
+    }
+    List<T> result = map.get(scope);
+    if (result == null) 
+    {
+      result = new LinkedList<T>();
+      map.put(scope, result);
+    }
+    return result;
+  }
 
 }
