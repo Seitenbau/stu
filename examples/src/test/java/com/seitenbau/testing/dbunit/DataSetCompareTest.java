6f92ce0b9ffc0b217adf0b6ebde31a08979f37f7
Dennis Kaulbersch <d.kaulbersch@gmail.com>
2013-06-04 10:06:27 +0200

    Addes GroovyDataSetProvider and added DataSetCompareTest

--- examples/src/test/java/com/seitenbau/testing/dbunit/DataSetCompareTest.java
+++ examples/src/test/java/com/seitenbau/testing/dbunit/DataSetCompareTest.java
@@ -7,6 +7,8 @@ import org.dbunit.dataset.IDataSet;
 import org.dbunit.dataset.ITable;
 import org.junit.Test;
 
+import com.seitenbau.testing.dbunit.dataset.DemoGroovyDataSet;
+import com.seitenbau.testing.dbunit.datasets.provider.impl.GroovyDataSetProvider;
 import com.seitenbau.testing.dbunit.datasets.provider.impl.JavaDataSetProvider;
 import com.seitenbau.testing.dbunit.datasets.provider.impl.STUDataSetProvider;
 
@@ -15,6 +17,8 @@ public class DataSetCompareTest
   JavaDataSetProvider javaDataSetProvider = new JavaDataSetProvider();
 
   STUDataSetProvider stuDataSetProvider = new STUDataSetProvider();
+  
+  GroovyDataSetProvider groovyDataSetProvider = new GroovyDataSetProvider();
 
   @Test
   public void compareJavaDataSetWithSTUDataSet() throws DataSetException
@@ -22,6 +26,7 @@ public class DataSetCompareTest
     // prepare
     IDataSet javaDataSet = javaDataSetProvider.getDataSet();
     IDataSet stuDataSet = stuDataSetProvider.getDataSet();
+    
     // verify
     assertThat(javaDataSet.getTableNames()).isEqualTo(stuDataSet.getTableNames());
 
@@ -29,6 +34,25 @@ public class DataSetCompareTest
     {
       ITable javaTable = javaDataSet.getTable(tableName);
       ITable stuTable = stuDataSet.getTable(tableName);
+      assertThat(javaTable.getRowCount()).isEqualTo(stuTable.getRowCount());
+      // TODO verify content
+    }
+  }
+  
+  @Test
+  public void compareJavaDataSetWithGroovyDataSet() throws DataSetException
+  {
+    // prepare
+    IDataSet javaDataSet = javaDataSetProvider.getDataSet();
+    IDataSet groovyDataSet = groovyDataSetProvider.getDataSet();
+    
+    // verify
+    assertThat(javaDataSet.getTableNames()).isEqualTo(groovyDataSet.getTableNames());
+
+    for (String tableName : javaDataSet.getTableNames())
+    {
+      ITable javaTable = javaDataSet.getTable(tableName);
+      ITable stuTable = groovyDataSet.getTable(tableName);
       assertThat(javaTable.getRowCount()).isEqualTo(stuTable.getRowCount());
       // TODO verify content
     }
---
+++ examples/src/test/java/com/seitenbau/testing/dbunit/datasets/provider/impl/GroovyDataSetProvider.java
@@ -0,0 +1,37 @@
+package com.seitenbau.testing.dbunit.datasets.provider.impl;
+
+import org.dbunit.dataset.DataSetException;
+import org.dbunit.dataset.IDataSet;
+
+import com.seitenbau.testing.dbunit.dataset.DemoGroovyDataSet;
+import com.seitenbau.testing.dbunit.datasets.provider.DataSetProvider;
+
+public class GroovyDataSetProvider implements DataSetProvider
+{
+
+  private IDataSet dataSet;
+
+  public GroovyDataSetProvider()
+  {
+    dataSet = createDataSet();
+  }
+
+  private IDataSet createDataSet()
+  {
+    return new DemoGroovyDataSet().createDataSet();
+  }
+
+  @Override
+  public IDataSet getDataSet() throws DataSetException
+  {
+    if (dataSet == null)
+    {
+      throw new DataSetException("No dataSet provided");
+    }
+    else
+    {
+      return dataSet;
+    }
+  }
+
+}
