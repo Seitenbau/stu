00e123b9297a3256e333691acc1277a29e2ed096
Nikolaus Moll <accounts@miblounge.net>
2013-06-06 09:16:22 +0200

    Improved ScopeRegistry (possible to remember last scope and re-set it), generated lazy value support code removed when unnecessary

--- database/src/main/java/com/seitenbau/testing/dbunit/dsl/ScopeRegistry.java
+++ database/src/main/java/com/seitenbau/testing/dbunit/dsl/ScopeRegistry.java
@@ -13,14 +13,26 @@ public final class ScopeRegistry
     throw new IllegalStateException("This class cannot be instantiated");
   }
 
-  public static void use(IScope scope)
+  /**
+   * Sets the current scope for DatabaseReference instances. Only References
+   * beloning to the scope's  will be affected.
+   * @param scope The scope to set
+   * @return The last active scope
+   */
+  public static IScope use(IScope scope)
   {
+    if (scope == null) {
+      return null;
+    }
+    
     ThreadLocal<IScope> activeScope = activeScopes.get(scope.getScopeIdentifier());
     if (activeScope == null) {
       activeScope = new ThreadLocal<IScope>();
       activeScopes.put(scope.getScopeIdentifier(), activeScope);
     }
+    IScope result = activeScope.get();
     activeScope.set(scope);
+    return result;
   }
 
   public static IScope getCurrentScope(String name)
