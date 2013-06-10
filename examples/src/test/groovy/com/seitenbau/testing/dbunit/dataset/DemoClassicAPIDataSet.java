e3a55b40478f6ccfc5985203d4c5111c4b2a28ba
Nikolaus Moll <accounts@miblounge.net>
2013-06-10 14:32:20 +0200

    removed hard coded ids in example data set

--- examples/src/test/groovy/com/seitenbau/testing/dbunit/dataset/DemoClassicAPIDataSet.java
+++ examples/src/test/groovy/com/seitenbau/testing/dbunit/dataset/DemoClassicAPIDataSet.java
@@ -12,15 +12,14 @@ public class DemoClassicAPIDataSet extends PersonDatabaseDataSet
   protected void initDataSet()
   {
     QA.personsTo(KAULBERSCH, GUITTON, BARANOWSKI);
-    
+
     table_Teams
       .insertRow()
         .bindRef(QA)
-        .setId(1)
         .setTitle("Quality Assurance")
         .setDescription("Verifies software")
         .setMembersize(new LazyValue() {
-  
+
           @Override
           public Object getValue()
           {
@@ -28,40 +27,34 @@ public class DemoClassicAPIDataSet extends PersonDatabaseDataSet
           }
           
         });
-    
+
     table_Jobs
       .insertRow()
         .bindRef(SWD)
-        .setId(1)
         .setTitle("Software Developer")
         .setDescription("Creating software")
       .insertRow()
         .bindRef(SWT)
-        .setId(2)
         .setTitle("Software Tester")
         .setDescription("Testing software")
       .insertRow()
         .bindRef(TM)
-        .setId(3)
         .setTitle("Team Manager")
         .setDescription("Makes the world go round");
 
     table_Persons
       .insertRow()
         .bindRef(KAULBERSCH)
-        .setId(1)
         .setFirstName("Dennis")
         .setName("Kaulbersch")
         .setJobId(SWD)
       .insertRow()
         .bindRef(GUITTON)
-        .setId(2)
         .setFirstName("Julien")
         .setName("Guitton")
         .setJobId(SWT)
       .insertRow()
         .bindRef(BARANOWSKI)
-        .setId(3)
         .setFirstName("Christian")
         .setName("Baranowski")
         .setJobId(TM);
