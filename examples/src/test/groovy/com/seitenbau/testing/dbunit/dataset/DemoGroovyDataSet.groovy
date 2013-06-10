36aa387dabfc1c5463a2d2655aa2667c187f4f4f
Nikolaus Moll <accounts@miblounge.net>
2013-06-10 14:22:48 +0200

    fixed DataSet for command line build

--- examples/src/test/groovy/com/seitenbau/testing/dbunit/dataset/DemoGroovyDataSet.groovy
+++ examples/src/test/groovy/com/seitenbau/testing/dbunit/dataset/DemoGroovyDataSet.groovy
@@ -4,31 +4,34 @@ import static com.seitenbau.testing.dbunit.PersonDatabaseRefs.*
 import com.seitenbau.testing.dbunit.model.dsl.*
 
 class DemoGroovyDataSet extends EmptyGroovyDataSet
-{{
-  relations {
-    QA.personsTo(KAULBERSCH, GUITTON, BARANOWSKI);
+{
+  public DemoGroovyDataSet() {
+    
+    relations {
+      QA.personsTo(KAULBERSCH, GUITTON, BARANOWSKI);
+    }
+  
+    tables {
+        
+      teamsTable.rows {
+        REF         | title                 | description         | membersize
+        QA          | "Quality Assurance"   | "Verifies software" | { personsTable.findWhere.teamId(QA).getRowCount() } 
+      }
+  
+      jobsTable.rows {
+        REF         | title                 | description
+        SWD         | "Software Developer"  | "Creating software"
+        SWT         | "Software Tester"     | "Testing software"
+        TM          | "Team Manager"        | "Makes the world go round"
+      }
+  
+      personsTable.rows {
+        REF         | first_name            | name                | job
+        KAULBERSCH  | "Dennis"              | "Kaulbersch"        | SWD
+        GUITTON     | "Julien"              | "Guitton"           | SWT
+        BARANOWSKI  | "Christian"           | "Baranowski"        | TM
+      }
+  
+    }
   }
-
-  tables {
-      
-    teamsTable.rows {
-      REF         | title                 | description         | membersize
-      QA          | "Quality Assurance"   | "Verifies software" | { personsTable.findWhere.teamId(QA).getRowCount() } 
-    }
-
-    jobsTable.rows {
-      REF         | title                 | description
-      SWD         | "Software Developer"  | "Creating software"
-      SWT         | "Software Tester"     | "Testing software"
-      TM          | "Team Manager"        | "Makes the world go round"
-    }
-
-    personsTable.rows {
-      REF         | first_name            | name                | job
-      KAULBERSCH  | "Dennis"              | "Kaulbersch"        | SWD
-      GUITTON     | "Julien"              | "Guitton"           | SWT
-      BARANOWSKI  | "Christian"           | "Baranowski"        | TM
-    }
-
-  }
-}}
+}
