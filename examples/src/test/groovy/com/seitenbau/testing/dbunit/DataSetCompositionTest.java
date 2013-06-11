db5d045a6040ac0e94db2f4de1d0363ee31f69ed
Nikolaus Moll <accounts@miblounge.net>
2013-06-11 17:19:12 +0200

    fixed DSL Builder template, added comments in composition tests

--- examples/src/test/groovy/com/seitenbau/testing/dbunit/DataSetCompositionTest.java
+++ examples/src/test/groovy/com/seitenbau/testing/dbunit/DataSetCompositionTest.java
@@ -6,7 +6,6 @@ import static org.fest.assertions.Assertions.assertThat;
 import org.dbunit.dataset.DataSetException;
 import org.fest.assertions.Fail;
 import org.junit.Test;
-import org.springframework.dao.DataIntegrityViolationException;
 
 import com.seitenbau.testing.dbunit.dataset.DemoGroovyDataSet;
 import com.seitenbau.testing.dbunit.dataset.ExtendedDemoGroovyDataSet;
@@ -18,7 +17,10 @@ public class DataSetCompositionTest
   @Test
   public void checkExtendedDemoGroovyDataSet() throws DataSetException
   {
+    // prepare
     ExtendedDemoGroovyDataSet dataSet = new ExtendedDemoGroovyDataSet();
+
+    // verify
     assertThat(dataSet.teamsTable.getRowCount()).isEqualTo(2);
     assertThat(dataSet.jobsTable.getRowCount()).isEqualTo(4);
     assertThat(dataSet.personsTable.getRowCount()).isEqualTo(3);
@@ -27,14 +29,21 @@ public class DataSetCompositionTest
   @Test
   public void checkValidRefUsage() throws DataSetException
   {
+    // prepare
     DataSetRegistry.use(new ExtendedDemoGroovyDataSet());
+
+    // verify
     assertThat(SAT.getTitle()).isEqualTo("Software Architect");
   }
 
   @Test(expected=RuntimeException.class)
   public void checkInvalidRefUsage() throws DataSetException
   {
+    // prepare
     DataSetRegistry.use(new DemoGroovyDataSet());
+
+    // verify
+    // throws a RuntimeException
     assertThat(SAT.getTitle()).isEqualTo("Software Architect");
 
     Fail.fail();
