8d757e5c79292c79552ce8231d8b504ad86d4dec
Nikolaus Moll <accounts@miblounge.net>
2013-06-12 10:15:09 +0200

    improved CastUtil

--- database/src/main/java/com/seitenbau/testing/dbunit/dsl/CastUtil.java
+++ database/src/main/java/com/seitenbau/testing/dbunit/dsl/CastUtil.java
@@ -8,77 +8,159 @@ public class CastUtil
 {
   public static Object cast(Object value, DataType type)
   {
-    if (type == null || type.getJavaTypeClass().isInstance(value))
+    if (type == null || value == null || type.getJavaTypeClass().isInstance(value))
     {
       return value;
     }
 
     switch (type)
     {
+    case UNKNOWN:
+      return value;
+      
+    case VARCHAR:
+    case CHAR:
+    case LONGVARCHAR:
+    case CLOB:
+      return castToString(value);
+      
+    case NUMERIC:
+    case DECIMAL:
+      return castToBigDecimal(value);
+      
+    case BOOLEAN:
+    case BIT:
+      return castToBoolean(value);
+      
     case INTEGER:
     case TINYINT:
     case SMALLINT:
-      if (value instanceof Long)
-      {
-        return ((Long) value).intValue();
-      }
-      if (value instanceof BigDecimal)
-      {
-        BigDecimal v = (BigDecimal) value;
-        return v.intValueExact();
-      }
-      break;
+      return castToInteger(value);
+
     case BIGINT:
-      if (value instanceof Integer)
-      {
-        return Long.valueOf((Integer) value);
-      }
-      if (value instanceof BigDecimal)
-      {
-        BigDecimal v = (BigDecimal) value;
-        return v.longValueExact();
-      }
-      break;
+      return castToLong(value);
+
     case REAL:
-      if (value instanceof Double)
-      {
-        Double d = (Double)value;
-        return d.floatValue();
-      }
-      if (value instanceof BigDecimal)
-      {
-        BigDecimal v = (BigDecimal) value;
-        return v.floatValue();
-      }
-      break;
+      return castToFloat(value);
+
     case DOUBLE:
     case FLOAT:
-      if (value instanceof Float)
-      {
-        return Double.valueOf((Float) value);
-      }
-      if (value instanceof Integer)
-      {
-        return Double.valueOf((Integer) value);
-      }
-      if (value instanceof Long)
-      {
-        return Double.valueOf((Long) value);
-      }
-      if (value instanceof BigDecimal)
-      {
-        BigDecimal v = (BigDecimal) value;
-        return v.doubleValue();
-      }
-      if (value instanceof String)
-      {
-        return Double.valueOf((String) value);
-      }
-      break;
+      return castToDouble(value);
+      
+    case DATE:
+    case TIME:
+    case TIMESTAMP:
+      return castToDate(value);
+      
+    case VARBINARY:
+    case BINARY:
+    case LONGVARBINARY:
+    case BLOB:    
+      return castToByteArray(value);
+      
     default:
-      return value;
+      throw new RuntimeException("Unhandled DataType in CastUtil: " + type.toString());
     }
-
+  }
+  
+  static Object castToString(Object value)
+  {
+    // TODO NM implement
     return value;
   }
+  
+  static Object castToBigDecimal(Object value)
+  {
+    // TODO NM implement
+    return value;
+  }
+  
+  static Object castToBoolean(Object value)
+  {
+    // TODO NM implement
+    return value;
+  }
+  
+  static Object castToInteger(Object value)
+  {
+    if (value instanceof Long)
+    {
+      return ((Long) value).intValue();
+    }
+    if (value instanceof BigDecimal)
+    {
+      BigDecimal v = (BigDecimal) value;
+      return v.intValueExact();
+    }    
+    return value;
+  }
+  
+  static Object castToLong(Object value)
+  {
+    if (value instanceof Integer)
+    {
+      return Long.valueOf((Integer) value);
+    }
+    if (value instanceof BigDecimal)
+    {
+      BigDecimal v = (BigDecimal) value;
+      return v.longValueExact();
+    }
+    return value;
+  }
+  
+  static Object castToFloat(Object value)
+  {
+    if (value instanceof Double)
+    {
+      Double d = (Double)value;
+      return d.floatValue();
+    }
+    if (value instanceof BigDecimal)
+    {
+      BigDecimal v = (BigDecimal) value;
+      return v.floatValue();
+    }
+    return value;
+  }
+
+  static Object castToDouble(Object value)
+  {
+    if (value instanceof Float)
+    {
+      return Double.valueOf((Float) value);
+    }
+    if (value instanceof Integer)
+    {
+      return Double.valueOf((Integer) value);
+    }
+    if (value instanceof Long)
+    {
+      return Double.valueOf((Long) value);
+    }
+    if (value instanceof BigDecimal)
+    {
+      BigDecimal v = (BigDecimal) value;
+      return v.doubleValue();
+    }
+    if (value instanceof String)
+    {
+      return Double.valueOf((String) value);
+    }
+    
+    return value;
+  }
+  
+  static Object castToDate(Object value)
+  {
+    // TODO NM implement
+    return value;
+  }
+  
+  static Object castToByteArray(Object value)
+  {
+    // TODO NM implement
+    return value;
+  }
+
 }
