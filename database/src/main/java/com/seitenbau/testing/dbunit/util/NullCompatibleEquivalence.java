214119eb213e482671b975dd503ded15b15b3b8a
Nikolaus Moll <accounts@miblounge.net>
2013-06-19 16:07:10 +0200

    improved NullCompatibleEquivalence and added tests

--- database/src/main/java/com/seitenbau/testing/dbunit/util/NullCompatibleEquivalence.java
+++ database/src/main/java/com/seitenbau/testing/dbunit/util/NullCompatibleEquivalence.java
@@ -1,37 +1,75 @@
 package com.seitenbau.testing.dbunit.util;
 
+import java.math.BigDecimal;
+
 public final class NullCompatibleEquivalence
 {
-  private NullCompatibleEquivalence() {
+  private NullCompatibleEquivalence()
+  {
   }
-  
-  public static boolean equals(Object value1, Object value2) {
-    if (value1 == null && value2 == null) {
+
+  public static boolean equals(Object value1, Object value2)
+  {
+    if (value1 == null && value2 == null)
+    {
       return true;
     }
-    
-    if (value1 == null || value2 == null) {
+
+    if (value1 == null || value2 == null)
+    {
       return false;
     }
-    
-    // both aren't null here
-    Long long1 = toLong(value1);
-    Long long2 = toLong(value2);
-    if (long1 != null && long2 != null) {
-      return long1.equals(long2);
+
+    // both aren't null here, build generalized values
+    Object generalizedValue1 = generalize(value1);
+    Object generalizedValue2 = generalize(value2);
+    if (generalizedValue1 != null && generalizedValue2 != null)
+    {
+      return generalizedValue1.equals(generalizedValue2);
     }
 
     return value1.equals(value2);
   }
-  
-  private static Long toLong(Object value) {
-    if (value instanceof Long) {
-      return (Long)value;
+
+  /**
+   * Brings an object to a more general representation of the object to be able to compare it with
+   * another value.
+   * An Long and an Integer value should be equal, even their type does not match.
+   * @param value The value which should be brought in a more general type
+   * @return The value in the same type as given and a more general if possible
+   */
+  static Object generalize(Object value)
+  {
+    if (value instanceof String)
+    {
+      return value;
     }
-    if (value instanceof Integer) {
-      return Long.valueOf((Integer)value);
+
+    if (value instanceof Long)
+    {
+      return BigDecimal.valueOf((Long)value);
     }
-    
-    return null;
+    if (value instanceof Integer)
+    {
+      return BigDecimal.valueOf((Integer)value);
+    }
+    if (value instanceof Double)
+    {
+      return BigDecimal.valueOf((Double)value);
+    }
+    if (value instanceof Float)
+    {
+      return BigDecimal.valueOf((Float)value);
+    }
+    if (value instanceof Short)
+    {
+      return BigDecimal.valueOf((Short)value);
+    }
+    if (value instanceof Byte)
+    {
+      return BigDecimal.valueOf((Byte)value);
+    }
+
+    return value;
   }
 }
