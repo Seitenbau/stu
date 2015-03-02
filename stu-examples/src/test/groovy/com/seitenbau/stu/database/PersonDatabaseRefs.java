a592fbeb1a39e1be53417f6a40a0274950d24126
RainerW <github@inforw.de>
2015-03-02 14:10:07 +0100

    fix new RefFactory name

--- stu-examples/src/test/groovy/com/seitenbau/stu/database/PersonDatabaseRefs.java
+++ stu-examples/src/test/groovy/com/seitenbau/stu/database/PersonDatabaseRefs.java
@@ -1,22 +1,22 @@
 package com.seitenbau.stu.database;
 
 import com.seitenbau.stu.database.model.JobsRef;
+import com.seitenbau.stu.database.model.PersonDatabaseRefFactory;
 import com.seitenbau.stu.database.model.PersonsRef;
-import com.seitenbau.stu.database.model.RefFactory;
 import com.seitenbau.stu.database.model.TeamsRef;
 
 public class PersonDatabaseRefs
 {
-  public static JobsRef SWD = RefFactory.createJobsRef();
-  public static JobsRef SWT = RefFactory.createJobsRef();
-  public static JobsRef TM = RefFactory.createJobsRef();
-  public static JobsRef SAT = RefFactory.createJobsRef();
+  public static JobsRef SWD = PersonDatabaseRefFactory.createJobsRef();
+  public static JobsRef SWT = PersonDatabaseRefFactory.createJobsRef();
+  public static JobsRef TM = PersonDatabaseRefFactory.createJobsRef();
+  public static JobsRef SAT = PersonDatabaseRefFactory.createJobsRef();
   
-  public static TeamsRef QA = RefFactory.createTeamsRef();
-  public static TeamsRef HR = RefFactory.createTeamsRef();
+  public static TeamsRef QA = PersonDatabaseRefFactory.createTeamsRef();
+  public static TeamsRef HR = PersonDatabaseRefFactory.createTeamsRef();
   
-  public static PersonsRef BARANOWSKI = RefFactory.createPersonsRef();
-  public static PersonsRef GUITTON = RefFactory.createPersonsRef();
-  public static PersonsRef KAULBERSCH = RefFactory.createPersonsRef();
-  public static PersonsRef HOCHLEITER = RefFactory.createPersonsRef();
+  public static PersonsRef BARANOWSKI = PersonDatabaseRefFactory.createPersonsRef();
+  public static PersonsRef GUITTON = PersonDatabaseRefFactory.createPersonsRef();
+  public static PersonsRef KAULBERSCH = PersonDatabaseRefFactory.createPersonsRef();
+  public static PersonsRef HOCHLEITER = PersonDatabaseRefFactory.createPersonsRef();
 }
